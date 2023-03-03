f78d5b52858681f0b0c92f21e59edf9e6ebd5225
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-03-03 08:53:46 -0500

    Nuevo diseño

--- src/ingresos/components/IngresosPage.jsx
+++ src/ingresos/components/IngresosPage.jsx
@@ -1,43 +1,37 @@
-
-import { useContext, useEffect, useState } from "react"
-import { UserContext } from "../context/UserContext"
-import { useFetch } from "../hooks/useFetch"
-import { useListadoIngresos } from "../hooks/useListadoIngresos"
-import { IngresosForm } from "./IngresosForm"
-import { IngresosListado } from "./IngresosListado"
-
-
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/UserContext";
+import { useFetch } from "../hooks/useFetch";
+import { useListadoIngresos } from "../hooks/useListadoIngresos";
+import { IngresosForm } from "./IngresosForm";
+import { IngresosListado } from "./IngresosListado";
 
 export const IngresosPage = () => {
+  const { url, listado } = useContext(UserContext);
 
-  const {url,listado} = useContext(UserContext)
+  const { data } = useFetch(url.url, url.data, url.method);
 
-  const {data} = useFetch(url.url,url.data,url.method)
-
-  useListadoIngresos(data,url.method)
+  useListadoIngresos(data, url.method);
 
   return (
-  <>
-    <section className="content">
-      <h1 className='content__title'>Control de i Personal</h1>
-      <IngresosForm />
-    </section>
-
-    <section className="table">
-    <IngresosListado datos={{
-        concepto:"Descripcion",
-        fecha:"Fecha",
-        valor:"Ingresos ($)",
-        encabezado:true
-      }}/> 
-    {
-        listado.map(
-          datos => 
-            <IngresosListado key={datos.id} datos={datos} />
-        )
-      }
-
-    </section>
-  </>
-  )
-}
+    <>
+      <section className="content">
+        <h1 className="content__title">
+          <span className="material-symbols-outlined">face</span> Control de i
+          Personal
+        </h1>
+        <IngresosForm />
+        <IngresosListado
+          datos={{
+            concepto: "Descripcion",
+            fecha: "Fecha",
+            valor: "Ingresos ($)",
+            encabezado: true,
+          }}
+        />
+        {listado.map((datos) => (
+          <IngresosListado key={datos.id} datos={datos} />
+        ))}
+      </section>
+    </>
+  );
+};
--- src/utils/NavBar.jsx
+++ src/utils/NavBar.jsx
@@ -1,41 +1,45 @@
-
-import { NavLink } from 'react-router-dom'
-import './styles.css'
-
+import { NavLink } from "react-router-dom";
+import "./styles.css";
 
 export const NavBar = () => {
-
   return (
-    <header style={{"width": "250px"}}>
-
-      <nav className="nav__principal">
-        <ul className='nav__ul'>
-          <div className='nav__content'>
-            <span className="material-symbols-outlined">
-              library_add
-            </span>
-            <li className='nav__item'>
-
+    <header>
+      <nav className="nav">
+        <div className="nav__content">
+          <span className="material-symbols-outlined">menu</span>
+          <span>Menú</span>
+        </div>
+        <ul className="nav__ul">
+          <div className="nav__content">
+            <span className="material-symbols-outlined">library_add</span>
+            <li className="nav__item">
               <NavLink
-                className={({ isActive }) => `${isActive ? 'NavLink__onclick' : ''}`}
-                to='/'
-              >Ingresos</NavLink>
+                className={({ isActive }) =>
+                  `${isActive ? "NavLink__onclick" : ""}`
+                }
+                to="/"
+              >
+                Ingresos
+              </NavLink>
             </li>
-
           </div>
-          <div className='nav__content'>
-            <span className="material-symbols-outlined">
-              delete_forever
-            </span>
-            <li className='nav__item'>
+          <hr />
+          <div className="nav__content">
+            <span className="material-symbols-outlined">delete_forever</span>
+            <li className="nav__item">
               <NavLink
-                className={({ isActive }) => `${isActive ? 'NavLink__onclick' : ''}`}
-                to='/demas'
-              >Gastos</NavLink>
+                className={({ isActive }) =>
+                  `${isActive ? "NavLink__onclick" : ""}`
+                }
+                to="/demas"
+              >
+                Gastos
+              </NavLink>
             </li>
           </div>
         </ul>
+        <span className="header__legend">&copy; Carlos Castro </span>
       </nav>
     </header>
-  )
-}
+  );
+};
